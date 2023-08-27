f2d9607972d7ead419981d5860cd66652f50c610
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-08-27 17:12:17 -0400

    controller ruta /driver

--- server/src/controllers/01-getDrivers.js
+++ server/src/controllers/01-getDrivers.js
@@ -1,7 +1,18 @@
 const axios = require("axios");
-const URL = require('../utils/url')
+const { Driver } = require('../db')
+const URL_API = require('../utils/url')
 
+//TODO Si no tiene imagen, colocarle una por defecto
+// Creo que aquí tendré que solicitarle a la BD también
 module.exports = async () => {
-    const response = await axios(URL)
-    return response.data
+    let allDrivers = []
+    
+    const DBDrivers = await Driver.findAll()
+    DBDrivers.length && DBDrivers.forEach(driver => allDrivers.push(driver))
+
+    const APIDrivers = await axios(URL_API)
+    APIDrivers.data.forEach(driver => allDrivers.push(driver))
+
+
+    return allDrivers
 }
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
         const response = await getDrivers()
         res.status(200).json(response)
     } catch (error) {
-        res.status(error.response.status).json({error: error.message})
+        res.status(404).json({error: error.message})
     }
 })
 
